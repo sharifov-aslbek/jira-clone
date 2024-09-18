257fe1c02fa2b9ae8d3d67fc439361cd0e8ca2ca
sharifov-aslbek <sharifovaslbek5@gmail.com>
2024-09-18 17:09:13 +0500

    Jira software main page ending 11/1 on 18/09/24

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <LayoutsMainNavbar />
+    <section class="min-h-screen bg-white dark:bg-black">
+        <slot/>
+    </section>
+</template>
